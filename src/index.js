ccf1dc33a04bc670cfcd9fd477fd9384305711e9
AdeolaFaraday <adexconly@gmail.com>
2021-04-14 05:34:19 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+	state = { lat: null, errorMessage: '' };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState({ lat: position.coords.latitude });
+			},
+			(err) => console.log(err)
+		);
+	}
+
+	render() {
+		return <SeasonDisplay lat={this.state.lat} />;
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
